c2eea3da7a7041d5513f3124d9c0e68dc22bec3b
T Heier <trentheier@gmail.com>
2021-04-06 13:50:37 -0400

    added the basic onclick functions for the search button and main window

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ var span = document.getElementsByClassName("close")[0];
 
 // this should open up the modal form
 searchBtn.onclick = function() {
-    modal.style.display = "block"
     console.log("this is a registered click")
 }
 
+// this function sees if they click on the screen , we need to add a conditional that when they click off of the modal it will close the modal. //
 
 window.onclick = function(closeModal) {
     console.log("this is another registered click")
