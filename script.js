f99d9e8508e435be57fb5cf4785810119a4d0d6e
T Heier <trentheier@gmail.com>
2021-04-06 13:33:24 -0400

    working on modal js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var modal = document.getElementsByClassName("modal");
 var searchBtn = document.getElementById("btn");
+var span = document.getElementsByClassName("close")[0];
 
 // this should open up the modal form
-searchBtn.onclick = function(modalOpen) {
+searchBtn.onclick = function() {
+    modal.style.display = "block"
     console.log("this is a registered click")
 }
+
 
 window.onclick = function(closeModal) {
     console.log("this is another registered click")
